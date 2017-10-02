55df1665204ac954435cb7c1dc0e41a0ce69acb1
habibaashraf <habiba_564@hotmail.com>
2017-10-02 23:01:44 +0200

    Task 8, second commit

--- music/urls.py
+++ music/urls.py
@@ -4,7 +4,18 @@ from . import views
 app_name = 'music'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<album_id>[0-9]+)/favorite$', views.favorite, name='favorite'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
+    # /music/album/add
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+
+    #/ music/album/2
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    # / music/album/2/delete
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 ]
